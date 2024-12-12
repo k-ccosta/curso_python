3f7d1258a88b61b22c8abdeb9a09cb1e7406eab4
k-ccosta <k.accosta98@gmail.com>
2024-12-11 20:46:58 -0400

    feat: adding imc category functionality

--- calculo_imc.py
+++ calculo_imc.py
@@ -4,4 +4,17 @@ peso = float(input("Digite seu peso em kg: "))
 
 imc = peso/(altura**2)
 
-print(f"{nome} tem {altura} de altura, pesa {peso} kg e seu IMC é {imc:.2f}")
+if imc < 18.5:
+    categoria = "abaixo do peso"
+elif imc < 24.9:
+    categoria = "peso normal"
+elif imc < 29.9:
+    categoria = "sobrepeso"
+elif imc < 34.9:
+    categoria = "obesidade grau I"
+elif imc < 39.9:
+    categoria = "obesidade grau II"
+else:
+    categoria = "obesidade grau III ou mórbida"
+
+print(f"{nome} tem {altura} de altura, pesa {peso} kg e seu IMC é {imc:.2f} | {categoria}")
